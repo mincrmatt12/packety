6720330d30ea80ad0244c4d08ce3a4081c4445d6
mincrmatt12 <matthewmirvish@hotmail.com>
2016-12-17 10:22:14 -0500

    Version bump to 0.0.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="packety",
-    version="0.0.6a3",
+    version="0.0.7",
     packages=find_packages(exclude=("tests")),
     install_requires=["gevent"],
 
@@ -12,5 +12,5 @@ setup(
     description="Simple packet based networking, with a side of async",
     long_description=open("readme.rst").read() if os.path.exists("readme.rst") else "Readme is missing, please get git source or read pypi",
     keywords="networking packet async",
-    changelog=
+    url="https://github.com/mincrmatt12/packety"
 )
