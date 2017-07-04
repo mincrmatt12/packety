9e32961ddce33e70d0f0f8d54645cb498d1fa896
mincrmatt12 <matthewmirvish@hotmail.com>
2017-07-04 18:45:34 -0400

    bump to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="packety",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=("tests")),
     install_requires=["gevent"],
     author="mincrmatt12",
